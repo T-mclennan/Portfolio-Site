e336d59327c017423a00147661fd6b42f93b7ad5
Tristan Mclennan <Tmclennan@mail.sfsu.edu>
2020-01-04 17:00:10 -0800

    initial commit for Portofolio project

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const items = require("./routes/api/items");
+const path = require("path");
+
+const app = express();
+
+//BodyParser middleware:
+app.use(bodyParser.json());
+
+//Serve static assets if in production:
+if (process.env.NODE_ENV === "production") {
+  //set static folder:
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    app.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Sever started on ${port}`));
