60db25c76bf6ca3b19691ccddfa7688e11372b05
Tristan Mclennan <Tmclennan@mail.sfsu.edu>
2020-01-07 15:41:17 -0800

    Styling sidebar with flexbox, setting placeholder content

--- client/src/components/Sidebar.js
+++ client/src/components/Sidebar.js
@@ -1,15 +1,27 @@
 import React, { Component } from "react";
+import headshot from "../assets/headshot.jpeg";
 import "../App.css";
 
 class Sidebar extends Component {
   render() {
     return (
       <div className="leftSidebar">
-        <div className="item">Left Sidebar</div>
-        <div className="item">pic</div>
-        <div className="item">Chess</div>
-        <div className="item">SFsafe</div>
-        <div className="item">Forms</div>
+        <img src={headshot} className="picture" alt="headshot" />
+        <div className="item">
+          <h4>Tristan Mclennan</h4>
+          <div>Junior Full-Stack Developer</div>
+        </div>
+        <div className="projects">
+          <div className="project">Chess 960</div>
+          <div className="project">SFSafe</div>
+          <div className="project">Forms</div>
+        </div>
+        <div className="media-container">
+          <div className="media">L </div>
+          <div className="media">G </div>
+          <div className="media">F </div>
+        </div>
+        {/* <div className="item">Forms</div> */}
       </div>
     );
   }
