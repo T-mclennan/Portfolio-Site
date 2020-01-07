661cc58e348cc3f9cdcc42a2726195e9e9584b1d
Tristan Mclennan <Tmclennan@mail.sfsu.edu>
2020-01-06 20:04:04 -0800

    beginning design of page layout, templating sidebar and main content

---
+++ client/src/components/Main.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import "../App.css";
+
+class Main extends Component {
+  render() {
+    return <div className="mainContent"> Main Content</div>;
+  }
+}
+
+export default Main;
