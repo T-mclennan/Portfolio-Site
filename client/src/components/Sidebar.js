70268d003d06348c19f26dc2ff8da526c2ea8b6a
Tristan Mclennan <Tmclennan@mail.sfsu.edu>
2020-01-07 16:23:46 -0800

    added fontAwesome icons for linkedin, github, facebook

--- client/src/components/Sidebar.js
+++ client/src/components/Sidebar.js
@@ -1,5 +1,13 @@
 import React, { Component } from "react";
 import headshot from "../assets/headshot.jpeg";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faLinkedin,
+  faFacebook,
+  faGithubSquare,
+  faFacebookSquare
+} from "@fortawesome/free-brands-svg-icons";
+import { Button } from "reactstrap";
 import "../App.css";
 
 class Sidebar extends Component {
@@ -17,9 +25,33 @@ class Sidebar extends Component {
           <div className="project">Forms</div>
         </div>
         <div className="media-container">
-          <div className="media">L </div>
-          <div className="media">G </div>
-          <div className="media">F </div>
+          <div className="media">
+            <a href={"https://www.linkedin.com/in/t-mclennan"}>
+              <FontAwesomeIcon
+                icon={faLinkedin}
+                className="icon far fa-2x fa-in"
+                size="2x"
+              />
+            </a>
+          </div>
+          <div className="media">
+            <a href={"https://www.github.com/T-mclennan"}>
+              <FontAwesomeIcon
+                icon={faGithubSquare}
+                className="icon far fa-2x fa-in"
+                size="2x"
+              />
+            </a>
+          </div>
+          <div className="media">
+            <a href={"https://www.facebook.com/tristan.mclennan.1"}>
+              <FontAwesomeIcon
+                icon={faFacebookSquare}
+                className="icon far fa-2x fa-in"
+                size="2x"
+              />
+            </a>
+          </div>
         </div>
         {/* <div className="item">Forms</div> */}
       </div>
