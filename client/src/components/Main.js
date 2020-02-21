bdc0575ae07b1a383a770cd79982a776af61bf21
Tristan Mclennan <Tmclennan@mail.sfsu.edu>
2020-02-21 15:19:52 -0800

    designed and placed content cards for each project

--- client/src/components/Main.js
+++ client/src/components/Main.js
@@ -30,9 +30,7 @@ class Main extends Component {
                 >
                   <h3>Github</h3>
                 </a>
-                <a>
-                  <h3>|</h3>
-                </a>
+                <h3 style={{ color: 'white' }}>|</h3>
                 <a href={'https://chess-960.herokuapp.com/'} target='_blank'>
                   <h3>Deployment</h3>
                 </a>
@@ -40,38 +38,55 @@ class Main extends Component {
             </div>
           </div>
 
-          {/* <div className="section-content">
-            <p>
-              This style of chess integrates an element of randomness into the
-              classic game, while still providing balance in the start
-              positions. There are a total of 960 unique opening positions
-              possible.
-            </p>
-
-            <p>On the left sidebar you'll find:</p>
-
-            <ul>
-              <li>
-                Quickstart: joins an open game, if none are available it will
-                create one.
-              </li>
-              <li>
-                New Game: creates a new game with the desired game options.
-              </li>
-              <li>
-                Find Game: Offers a list of available games for you to join.
-              </li>
-              <li>Standings: Shows the current score of all top players.</li>
-            </ul>
-          </div> */}
+          <div className='project-details description-top chess-top'>
+            <h2>Description:</h2>
+            <hr />
+            <h5>
+              Real-time chess application with randomized start positions. A
+              board generation algorithm ensures that there is a relative
+              balance between players, and given these constraints there are 960
+              possible start positions.
+            </h5>
+          </div>
         </section>
 
         <section id='chess2'>
-          <p>
-            This style of chess integrates an element of randomness into the
-            classic game, while still providing balance in the start positions.
-            There are a total of 960 unique opening positions possible.
-          </p>
+          <div className='project-details description-bottom chess-bottom'>
+            <h2>Architecture:</h2>
+            <hr />
+            <h5>
+              <ul>
+                <li>Node.js with Express for backend.</li>
+                <li>React, Redux, Reactstrap, CSS.</li>
+                <li>JWT for Authentication.</li>
+                <li>MongoDB Atlas for storage.</li>
+                <li>Socket.io for real time data transfer.</li>
+              </ul>
+            </h5>
+            <hr />
+            <h2>Features:</h2>
+            <hr />
+            <h5>
+              <ul>
+                <li>GUI for Lobby, Game creation, and Game logic.</li>
+                <li>
+                  Players can create a new game with any specified options,
+                  either 960 or regular.
+                </li>
+                <li>
+                  Players can see and re-join past games from the dashboard.
+                </li>
+                <li>
+                  Quickgame action joins an open game, if none are available it
+                  will create and join a generic new game.
+                </li>
+                <li>
+                  Find Game action offers a list of available games to join.
+                </li>
+                <li>View of online players in lobby real time.</li>
+              </ul>
+            </h5>
+          </div>
         </section>
 
         <section id='sfsafe'>
@@ -98,15 +113,51 @@ class Main extends Component {
               </div>
             </div>
           </div>
+          <div className='project-details description-top sfsafe-top'>
+            <h2>Description:</h2>
+            <hr />
+            <h5>
+              Real-time chess application with randomized start positions. A
+              board generation algorithm ensures that there is a relative
+              balance between players, and given these constraints there are 960
+              possible start positions.
+            </h5>
+          </div>
         </section>
 
         <section id='sfsafe2'>
-          <p>
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Culpa
-            error quaerat dolore alias libero temporibus dolorum maiores
-            officiis sunt dignissimos nulla molestias, unde in atque consectetur
-            architecto ullam et sit.
-          </p>
+          <div className='project-details description-bottom sfsafe-bottom'>
+            <h2>Architecture:</h2>
+            <hr />
+            <h5>
+              <ul>
+                <li>Node.js with Express for backend</li>
+                <li>React, Redux, Reactstrap, CSS</li>
+                <li>JWT for Authentication</li>
+                <li>MongoDB Atlas for storage</li>
+                <li>Socket.io for real time data transfer</li>
+              </ul>
+            </h5>
+            <hr />
+            <h2>Features:</h2>
+            <hr />
+            <h5>
+              <ul>
+                <li>GUI for Lobby, Game creation, and Game logic</li>
+                <li>
+                  Players can create a new game with specified options, either
+                  960 style or regular.
+                </li>
+                <li>Players can see and rejoin past games from dashboard</li>
+                <li>
+                  Quickgame action joins an open game, if none are available it
+                  will create one.
+                </li>
+                <li>Find game action offers a list of open games to join</li>
+                <li>View of online players in lobby real time</li>
+              </ul>
+            </h5>
+          </div>
         </section>
 
         <section id='forms'>
@@ -138,13 +189,58 @@ class Main extends Component {
             </div>
           </div>
 
-          <br />
-          <p>
+          {/* <p>
             Forms practice aims to develop professionalism around the use of
             forms. - Node and Express for the server - EJS was used to template
             the pages - Bootstrap and CSS for styling - Costum regex for
             validation - reCaptcha 2.0 - Google map and geolocation services
-          </p>
+          </p> */}
+
+          <div className='project-details description-top forms-top'>
+            <h2>Description:</h2>
+            <hr />
+            <h5>
+              Real-time chess application with randomized start positions. A
+              board generation algorithm ensures that there is a relative
+              balance between players, and given these constraints there are 960
+              possible start positions.
+            </h5>
+          </div>
+        </section>
+
+        <section id='forms2'>
+          <div className='project-details description-bottom forms-bottom'>
+            <h2>Architecture:</h2>
+            <hr />
+            <h5>
+              <ul>
+                <li>Node.js with Express for backend</li>
+                <li>React, Redux, Reactstrap, CSS</li>
+                <li>JWT for Authentication</li>
+                <li>MongoDB Atlas for storage</li>
+                <li>Socket.io for real time data transfer</li>
+              </ul>
+            </h5>
+            <hr />
+            <h2>Features:</h2>
+            <hr />
+            <h5>
+              <ul>
+                <li>GUI for Lobby, Game creation, and Game logic</li>
+                <li>
+                  Players can create a new game with specified options, either
+                  960 style or regular.
+                </li>
+                <li>Players can see and rejoin past games from dashboard</li>
+                <li>
+                  Quickgame action joins an open game, if none are available it
+                  will create one.
+                </li>
+                <li>Find game action offers a list of open games to join</li>
+                <li>View of online players in lobby real time</li>
+              </ul>
+            </h5>
+          </div>
         </section>
       </div>
     );
