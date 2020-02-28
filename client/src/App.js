d927aea73c09f904d1503bbda08ce8e9918a8cd2
Tristan Mclennan <Tmclennan@mail.sfsu.edu>
2020-02-28 01:36:44 -0800

    cleaning up and re-styling code

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,12 @@
-import React, { Component } from "react";
-import { Container } from "reactstrap";
-import Main from "./components/Main";
-import Sidebar from "./components/Sidebar";
-import { Link, animateScroll as scroll } from "react-scroll";
-import "./App.css";
+import React, { Component } from 'react';
+import Main from './components/Main';
+import Sidebar from './components/Sidebar';
+import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div className='App'>
         <Sidebar />
         <Main />
       </div>
--- client/src/components/Sidebar.js
+++ client/src/components/Sidebar.js
@@ -1,72 +1,66 @@
-import React, { Component } from "react";
-import headshot from "../assets/headshot.jpeg";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React, { Component } from 'react';
+import headshot from '../assets/headshot.jpeg';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {
   faLinkedin,
-  faFacebook,
   faGithubSquare,
-  faFacebookSquare
-} from "@fortawesome/free-brands-svg-icons";
-import { Button } from "reactstrap";
-import "../App.css";
+  faFacebookSquare,
+} from '@fortawesome/free-brands-svg-icons';
+import '../App.css';
 
 class Sidebar extends Component {
   render() {
     return (
-      <div className="leftSidebar">
-        <a href="#home">
-          <img src={headshot} className="picture" alt="headshot" />
+      <div className='leftSidebar'>
+        <a href='#home'>
+          <img src={headshot} className='picture' alt='headshot' />
         </a>
-        <div className="item">
+        <div className='item'>
           <h4>Tristan Mclennan</h4>
           <div>Junior Full-Stack Developer</div>
         </div>
-        <div className="projects">
+        <div className='projects'>
           <ul>
             <li>
-              <a href="#chess">Chess 960</a>
+              <a href='#chess'>Chess 960</a>
             </li>
             <li>
-              <a href="#sfsafe">Safe SF</a>
+              <a href='#sfsafe'>Safe SF</a>
             </li>
             <li>
-              <a href="#forms">Form Project</a>
+              <a href='#forms'>Form Project</a>
             </li>
           </ul>
-          {/* <li className="project">Chess 960</li>
-          <div className="project">SFSafe</div>
-          <div className="project">Forms</div> */}
         </div>
-        <div className="media-container">
-          <div className="media">
-            <a href={"https://www.linkedin.com/in/t-mclennan"}>
+        <div className='media-container'>
+          <div className='media'>
+            <a href={'https://www.linkedin.com/in/t-mclennan'}>
               <FontAwesomeIcon
                 icon={faLinkedin}
-                className="icon far fa-2x fa-in"
-                size="2x"
+                className='icon far fa-2x fa-in'
+                size='2x'
               />
             </a>
           </div>
-          <div className="media">
-            <a href={"https://www.github.com/T-mclennan"}>
+          <div className='media'>
+            <a href={'https://www.github.com/T-mclennan'}>
               <FontAwesomeIcon
                 icon={faGithubSquare}
-                className="icon far fa-2x fa-in"
-                size="2x"
+                className='icon far fa-2x fa-in'
+                size='2x'
               />
             </a>
           </div>
-          <div className="media">
-            <a href={"https://www.facebook.com/tristan.mclennan.1"}>
+          <div className='media'>
+            <a href={'https://www.facebook.com/tristan.mclennan.1'}>
               <FontAwesomeIcon
                 icon={faFacebookSquare}
-                className="icon far fa-2x fa-in"
-                size="2x"
+                className='icon far fa-2x fa-in'
+                size='2x'
               />
             </a>
           </div>
         </div>
-        {/* <div className="item">Forms</div> */}
       </div>
     );
   }
